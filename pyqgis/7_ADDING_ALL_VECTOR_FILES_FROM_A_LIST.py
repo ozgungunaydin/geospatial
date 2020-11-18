7142b499e3bbbbd219a498cc749bea28f510dcfd
oznotthewizard <54985230+oznotthewizard@users.noreply.github.com>
2020-11-18 22:03:31 +0300

    Delete 7_ADDING_ALL_VECTOR_FILES_FROM_A_LIST.py

--- pyqgis/7_ADDING_ALL_VECTOR_FILES_FROM_A_LIST.py
+++
@@ -1,9 +0,0 @@
-# ADDING ALL VECTOR FILES TO PROJECT FROM A LIST
-# ____________________________________________________________________________________________________________
-# iterate through list of paths to be used
-for building_shp in all_buildings_shp:
-    # assign name variable to current item
-    name = building_shp.replace('.shp', '')
-    # add current vector to the project (path, name, ogr library (for shapefile and many other file formats))
-    vlayer = iface.addVectorLayer(building_shp, name, 'ogr')
-# ____________________________________________________________________________________________________________
